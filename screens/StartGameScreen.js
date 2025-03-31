c8dfb9298dfe3e695b783a25134debdbd601413f
Sanskar Gupta <sanskar.gupta@getir.com>
2025-04-01 00:07:33 +0530

    building coustum buttons and layout for input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,9 +4,22 @@ import PrimaryButton from '../components/PrimaryButton';
 const StartGameScreen = () => {
     return (
         <View style={styles.inputContainer}>
-            <TextInput />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <TextInput 
+                style={styles.numberInput}
+                maxLength={2} 
+                keyboardType="number-pad"
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
+            
         </View>
     );
 };
@@ -19,13 +32,34 @@ const styles = StyleSheet.create({
         marginHorizontal: 24,
         borderRadius: 8,
         padding: 16,
-        backgroundColor: '#72063c',
+        backgroundColor: '#4e0329',
         //Shadow for ios & Android
         elevation: 8, //Android only property
         //Ios shadow property
         shadowColor: 'black',
         shadowOffset: { width: 0, height: 4 },
         shadowRadius: 6,
-        shadowOpacity: 0.5
+        shadowOpacity: 0.5,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    numberInput: {
+        textAlign: 'center',
+        width: '10%',
+        height: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold'
+    },
+    buttonsContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    buttonContainer: {
+        flex: 1
     }
 });
